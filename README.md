d2d4949fc7e4f4f5c95b2775f0bc534d7dda7142
Kyle Alvares <kyle@Kyles-MacBook-Air.local>
2022-10-05 15:35:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Modern YouTube
+
+An extension to beautify YouTube on chromium browsers.
+
+## Installation 
+
+Instructions provided are for Microsoft Edge, search up your browser to load an unpacked extension.
+
+1. *Clone* or *Download as ZIP and extract* repository.
+1. Open your browsers extensions pages (ex. <edge://extensions>).
+1. Enable *developer mode*.
+1. Click *load unpacked*.
+1. Select the previously downloaded folder.
+1. Congratulations! You now have a better Youtube!
---
+++ content.js
@@ -0,0 +1,19 @@
+const searchSVG = '<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>';
+const micSVG = '<svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="32" height="32" viewBox="0 0 32 32" aria-hidden="true"><path d="M23,14v3A7,7,0,0,1,9,17V14H7v3a9,9,0,0,0,8,8.94V28H11v2H21V28H17V25.94A9,9,0,0,0,25,17V14Z"></path><path d="M16,22a5,5,0,0,0,5-5V7A5,5,0,0,0,11,7V17A5,5,0,0,0,16,22Z"></path><title>Microphone filled</title></svg>';
+const createSVG = '<svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="32" height="32" viewBox="0 0 32 32" aria-hidden="true"><path d="M18 15L14 15 14 11 12 11 12 15 8 15 8 17 12 17 12 21 14 21 14 17 18 17 18 15z"></path><path d="M21,26H4a2.0023,2.0023,0,0,1-2-2V8A2.0023,2.0023,0,0,1,4,6H21a2.0023,2.0023,0,0,1,2,2v4.0566l5.4189-3.87A.9995.9995,0,0,1,30,9V23a.9995.9995,0,0,1-1.5811.8135L23,19.9434V24A2.0023,2.0023,0,0,1,21,26ZM4,8V24.001L21,24V18a.9995.9995,0,0,1,1.5811-.8135L28,21.0566V10.9434l-5.4189,3.87A.9995.9995,0,0,1,21,14V8Z"></path><title>Video add</title></svg>';
+const bellSVG = '<svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="32" height="32" viewBox="0 0 32 32" aria-hidden="true"><path d="M28.7071,19.293,26,16.5859V13a10.0136,10.0136,0,0,0-9-9.9492V1H15V3.0508A10.0136,10.0136,0,0,0,6,13v3.5859L3.2929,19.293A1,1,0,0,0,3,20v3a1,1,0,0,0,1,1h7v.7768a5.152,5.152,0,0,0,4.5,5.1987A5.0057,5.0057,0,0,0,21,25V24h7a1,1,0,0,0,1-1V20A1,1,0,0,0,28.7071,19.293ZM19,25a3,3,0,0,1-6,0V24h6Zm8-3H5V20.4141L7.707,17.707A1,1,0,0,0,8,17V13a8,8,0,0,1,16,0v4a1,1,0,0,0,.293.707L27,20.4141Z"></path><title>Notification</title></svg>';
+
+onload = (event) => {
+
+    // document.querySelector('#voice-search-button yt-icon').innerHTML = micSVG; // mic search
+
+    headButtons = document.querySelectorAll('.ytd-masthead#buttons yt-icon');
+    if(headButtons.length > 1) {
+        headButtons[0].innerHTML = createSVG; // create button
+        headButtons[1].innerHTML = bellSVG; // notifications button 
+    }
+
+    if(document.querySelector('yt-icon#search-icon')) 
+        document.querySelector('yt-icon#search-icon').innerHTML = searchSVG; // search
+    
+} 
